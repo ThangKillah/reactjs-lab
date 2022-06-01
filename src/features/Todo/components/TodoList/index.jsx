5ddf9a1855de3219f172faf271f19120aaafb267
ThangBT <thangbt1307@gmail.com>
2022-06-01 22:18:02 +0700

    change todo list using state

--- src/features/Todo/components/TodoList/index.jsx
+++ src/features/Todo/components/TodoList/index.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import classnames from "classnames";
 import PropTypes from "prop-types";
-import TodoItem from "../TodoItem";
+import React from "react";
+import "./style.scss";
 
 TodoList.propTypes = {
   todoList: PropTypes.array.isRequired,
+  onClickTodo: PropTypes.func,
 };
 
-function TodoList({ todoList }) {
+function TodoList({ todoList, onClickTodo }) {
+  const handleClickOnTodo = (index) => {
+    if (!onClickTodo) return;
+    console.log("click test");
+    onClickTodo(index);
+  };
+
   return (
-    <div>
-      {todoList.map((todo) => (
-        <TodoItem todo={todo} />
+    <ul className="todo-list">
+      {todoList.map((todo, index) => (
+        <li
+          onClick={() => handleClickOnTodo(index)}
+          key={todo.id}
+          className={classnames("test", {
+            completed: todo.status === "completed",
+          })}
+        >
+          {todo.title}
+        </li>
       ))}
-    </div>
+    </ul>
   );
 }
 
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "./components/TodoList";
 
 TodoFeature.propTypes = {};
 
 function TodoFeature(props) {
-  const todoList = [
-    { id: 1, title: "Diablo" },
-    { id: 2, title: "Diablo 2" },
-    { id: 3, title: "Diablo 3" },
-    { id: 4, title: "Diablo 4" },
+  const todoListInit = [
+    { id: 1, title: "Diablo", status: "new" },
+    { id: 2, title: "Diablo 2", status: "new" },
+    { id: 3, title: "Diablo 3", status: "completed" },
+    { id: 4, title: "Diablo 4", status: "completed" },
   ];
+
+  const [todoList, setTodoList] = useState(todoListInit);
+
+  const toggleTodo = (id) => {
+    const newTodoList = [...todoList];
+    const status = newTodoList[id].status;
+    newTodoList[id].status = status === "new" ? "completed" : "new";
+    setTodoList(newTodoList);
+  };
 
   return (
     <div>
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onClickTodo={toggleTodo} />
     </div>
   );
 }
